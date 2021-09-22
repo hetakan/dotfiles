a41099a3ab9af67cc941e35b42b4c1c9c5866bbf
hetakan <hetakan@gmail.com>
2021-09-22 18:07:34 +0900

    Check 'source' command

--- install_git_completion.sh
+++ install_git_completion.sh
@@ -3,8 +3,7 @@
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 chmod a+x ~/.git-completion.bash
 
-if type source > /dev/null 2>&1
-then
+if type source > /dev/null 2>&1; then
   echo "souce ~/.git-completion.bash" >> ~/.bashrc
   source ~/.bashrc
 else
--- install_git_prompt.sh
+++ install_git_prompt.sh
@@ -3,8 +3,7 @@
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
 chmod a+x ~/.git-prompt.sh
 
-if type source > /dev/null 2>&1
-then
+if type source > /dev/null 2>&1; then
   echo "source ~/.git-prompt.sh" >> ~/.bashrc
   source ~/.bashrc
 else
