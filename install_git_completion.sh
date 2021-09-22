8fa1e9e812899a84390eb60671423598b696283f
hetakan <hetakan@gmail.com>
2021-09-22 18:09:57 +0900

    typo

--- install_git_completion.sh
+++ install_git_completion.sh
@@ -4,7 +4,7 @@ wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-com
 chmod a+x ~/.git-completion.bash
 
 if type source > /dev/null 2>&1; then
-  echo "souce ~/.git-completion.bash" >> ~/.bashrc
+  echo "source ~/.git-completion.bash" >> ~/.bashrc
   source ~/.bashrc
 else
   echo ". ~/.git-completion.bash" >> ~/.bashrc
