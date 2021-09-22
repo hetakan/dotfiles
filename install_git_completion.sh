a35711afa4d8188bd86e50278085b894babbd038
hetakan <hetakan@gmail.com>
2021-09-22 18:11:56 +0900

    かっこを入れてみる

--- install_git_completion.sh
+++ install_git_completion.sh
@@ -3,7 +3,7 @@
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 chmod a+x ~/.git-completion.bash
 
-if type source > /dev/null 2>&1; then
+if (type source > /dev/null 2>&1); then
   echo "source ~/.git-completion.bash" >> ~/.bashrc
   source ~/.bashrc
 else
