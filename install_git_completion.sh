5f5d1bc41b61838f8a2af5d1cfa6dc291f6a06c4
hetakan <hetakan@gmail.com>
2021-09-22 18:00:48 +0900

    Check 'source' command

--- install_git_completion.sh
+++ install_git_completion.sh
@@ -3,5 +3,11 @@
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 chmod a+x ~/.git-completion.bash
 echo "source ~/.git-completion.bash" >> ~/.bashrc
-source ~/.bashrc
+
+if type source > /dev/null 2>&1
+then
+  source ~/.bashrc
+else
+  . ~/.bashrc
+fi
 
--- install_git_prompt.sh
+++ install_git_prompt.sh
@@ -3,6 +3,12 @@
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
 chmod a+x ~/.git-prompt.sh
 echo "source ~/.git-prompt.sh" >> ~/.bashrc
-# ~/.bashrc内のPS1 変数を __git_ps1 を含むように変更する
-source ~/.bashrc
+
+if type source > /dev/null 2>&1
+then
+  source ~/.bashrc
+else
+  . ~/.bashrc
+fi
+
 
