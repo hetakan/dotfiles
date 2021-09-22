08fa0e61f6bc95324a7f754c9115d48a361e83fb
hetakan <hetakan@gmail.com>
2021-09-22 19:04:26 +0900

    再チャレンジ

--- install_git_prompt.sh
+++ install_git_prompt.sh
@@ -2,15 +2,6 @@
 
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
 chmod a+x ~/.git-prompt.sh
-# echo ". ~/.git-prompt.sh" >> ~/.bashrc
+echo '. ~/.git-prompt.sh' >> ~/.bashrc
 . ~/.bashrc
-
-# if type source > /dev/null 2>&1; then
-#   echo "source ~/.git-prompt.sh" >> ~/.bashrc
-#   source ~/.bashrc
-# else
-#   echo ". ~/.git-prompt.sh" >> ~/.bashrc
-#   . ~/.bashrc
-# fi
-
 
